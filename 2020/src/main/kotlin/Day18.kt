d33bb7fed77bc3008ad2534cecf45f944ef1c965
Kevin Cheung <kevincheung@google.com>
2020-12-18 15:44:44 -0800

    Day 18 -- use pt2 soln for pt1

--- 2020/src/main/kotlin/Day18.kt
+++ 2020/src/main/kotlin/Day18.kt
@@ -1,4 +1,47 @@
-class Day18 {
+class Expression(private var expression: String) {
+    private val exprStack = mutableListOf<Long>()
+
+    fun eval(): Long {
+        evalNextExpr()
+        return exprStack.removeAt(exprStack.lastIndex)
+    }
+
+    private fun evalNextExpr() {
+        evalNextTerm()
+        while (expression.isNotEmpty() && expression.first() in setOf('+', '*')) {
+            when (expression.first()) {
+                '+' -> {
+                    expression = expression.substring(1).trim()
+                    evalNextTerm()
+                    val t1 = exprStack.removeAt(exprStack.lastIndex)
+                    val t2 = exprStack.removeAt(exprStack.lastIndex)
+                    exprStack.add(t1 + t2)
+                }
+                '*' -> {
+                    expression = expression.substring(1).trim()
+                    evalNextTerm()
+                    val t1 = exprStack.removeAt(exprStack.lastIndex)
+                    val t2 = exprStack.removeAt(exprStack.lastIndex)
+                    exprStack.add(t1 * t2)
+                }
+            }
+        }
+    }
+
+    private fun evalNextTerm() {
+        when {
+            expression.first() in '0'..'9' -> {
+                val num = expression.takeWhile { it in '0'..'9' }
+                expression = expression.substring(num.length).trim()
+                exprStack.add(num.toLong())
+            }
+            expression.first() == '(' -> {
+                expression = expression.substring(1).trim()
+                evalNextExpr()
+                expression = expression.substring(1).trim()  // ')'
+            }
+        }
+    }
 }
 
 class ExpressionV2(private var expression: String) {
@@ -47,62 +90,10 @@ class ExpressionV2(private var expression: String) {
     }
 }
 
-fun eval(expression: String): Long {
-    return try {
-        expression.toLong()
-    } catch (e: NumberFormatException) {
-        val (head, tailExpr) = getTrailingExpression(expression)
-        val (headExpr, operator) = getTrailingOperator(head)
-        when (operator) {
-            "+" -> eval(headExpr) + eval(tailExpr)
-            "*" -> eval(headExpr) * eval(tailExpr)
-            "" -> eval(tailExpr)
-            else -> throw Exception()
-        }
-    }
-}
-
-fun getTrailingExpression(expression: String): Pair<String, String> {
-    val expression = expression.trim()
-    return when {
-        expression.last() in '0'..'9' -> {
-            val tail = expression.takeLastWhile { it in '0'..'9' }
-            val head = expression.substring(0, expression.length - tail.length)
-            Pair(head, tail)
-        }
-        expression.last() == ')' -> {
-            var unclosed = 1
-            var i = expression.lastIndex - 1
-            while (unclosed > 0) {
-                when (expression[i]) {
-                    '(' -> unclosed -= 1
-                    ')' -> unclosed += 1
-                    else -> {}
-                }
-                i -= 1
-            }
-            val tail = expression.substring(i + 2, expression.length - 1)
-            val head = expression.substring(0, i + 1)
-            Pair(head, tail)
-        }
-        else -> throw Exception()
-    }
-}
-
-fun getTrailingOperator(exprFrag: String): Pair<String, String> {
-    val exprFrag = exprFrag.trim()
-    return when {
-        exprFrag == "" -> Pair("", "")
-        exprFrag.last() in setOf('*', '+') -> Pair(exprFrag.substring(0, exprFrag.length - 1),
-                                                   exprFrag.substring(exprFrag.length - 1))
-        else -> throw Exception()
-    }
-}
-
 fun main() {
-    val input = Day18::javaClass.javaClass.classLoader.getResource("day18.txt")?.readText() ?: ""
+    val input = Expression::javaClass.javaClass.classLoader.getResource("day18.txt")?.readText() ?: ""
 
-    val pt1sum = input.split("\n").map { eval(it) }.sum()
+    val pt1sum = input.split("\n").map { Expression(it).eval() }.sum()
     println(pt1sum)
 
     val pt2sum = input.split("\n").map { ExpressionV2(it).eval() }.sum()
--- 2020/src/test/kotlin/Day18Tests.kt
+++ 2020/src/test/kotlin/Day18Tests.kt
@@ -2,33 +2,10 @@ import org.junit.jupiter.api.Test
 
 class Day18Tests {
     @Test
-    fun testGetTrailingExpression() {
-        val (head, tail) = getTrailingExpression("1 + 2 * 3 + 4 * 5 + 6")
-        assert(head == "1 + 2 * 3 + 4 * 5 + ")
-        assert(tail == "6")
-
-        val (head2, tail2) = getTrailingExpression("1 + (2 * 3) + (4 * (5 + 6))")
-        assert(head2 == "1 + (2 * 3) + ")
-        assert(tail2 == "4 * (5 + 6)")
-    }
-
-    @Test
-    fun testGetTrailingOperator() {
-        val (head, tail) = getTrailingOperator("1 + 2 * 3 + 4 * 5 + ")
-        assert(head == "1 + 2 * 3 + 4 * 5 ")
-        assert(tail == "+")
-
-        val (head2, tail2) = getTrailingOperator("1 + (2 * 3) + ")
-        assert(head2 == "1 + (2 * 3) ")
-        assert(tail2 == "+")
-    }
-
-    @Test
     fun pt1() {
-        val ex1 = eval("1 + 2 * 3 + 4 * 5 + 6")
-        assert(ex1 == 71L)
-//        val ex6 = eval("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
-//        assert(ex1 == 13632L)
+        assert(Expression("1 + 2 * 3 + 4 * 5 + 6").eval() == 71L)
+        assert(Expression("1 + (2 * 3) + (4 * (5 + 6))").eval() == 51L)
+        assert(Expression("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2").eval() == 13632L)
     }
 
     @Test
