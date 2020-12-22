2719fb1947dc628e5ac48ad9ec86cf6e87f0cadf
Kevin Cheung <kevincheung@google.com>
2020-12-21 16:09:54 -0800

    Day 19, pt.2

--- 2020/src/main/kotlin/Day19.kt
+++ 2020/src/main/kotlin/Day19.kt
@@ -1,58 +1,110 @@
 sealed class MessageRule
-data class CharRule(val char: Char): MessageRule()
-data class SequenceRule(val ruleSequences: List<List<Int>>): MessageRule()
-typealias RuleSet = MutableMap<Int, MessageRule>
+data class ResolvedRule(val validMsgs: List<String>): MessageRule()
+data class UnresolvedRule(val ruleSequences: List<List<Int>>): MessageRule()
+typealias MessageRuleSet = MutableMap<Int, MessageRule>
 
-fun RuleSet.satisfiesRule(ruleIdx: Int, string: String) =
-    this.match(ruleIdx, string) == ""
-
-fun RuleSet.match(ruleIdx: Int, string: String): String? {
-    if (string == "") return null
-    val rule = this[ruleIdx]
-    return when {
-        (rule is CharRule) -> {
-            if (string[0] == rule.char) string.substring(1) else null
+fun MessageRuleSet.resolve(idx: Int) {
+    val rule = this[idx]
+    if (rule is UnresolvedRule) {
+        val validMsgs = mutableSetOf<String>()
+        rule.ruleSequences.forEach { seq ->
+            var validSeqs = listOf("")
+            seq.forEach { r ->
+                this.resolve(r)
+                val rr = this[r] as ResolvedRule
+                validSeqs = validSeqs.combineWith(rr.validMsgs)
+            }
+            validMsgs.addAll(validSeqs)
+            validMsgs.remove("")
         }
-        (rule is SequenceRule) -> {
-            rule.ruleSequences.forEach { seq ->
-                var theRest: String? = string
-                for (r in seq) {
-                    if (theRest == null) {
-                        break
-                    }
-                    theRest = this.match(r, theRest)
-                    print("")
-                }
-                if (theRest != null) {
-                    return theRest
+        this[idx] = ResolvedRule(validMsgs.toList())
+    }
+}
+
+fun List<String>.combineWith(that: List<String>): List<String> =
+    this.map { str -> that.map { str + it } }.flatten()
+
+fun String.isEchoOf(shortStr: String): Boolean = when (this) {
+    shortStr -> true
+    else -> this.startsWith(shortStr) && this.substring(shortStr.length).isEchoOf(shortStr)
+}
+
+fun String.isValidByRule0(valid42s: List<String>, valid31s: List<String>): Boolean {
+    for (i in 1 until this.length - 1) {
+        val prefix = this.substring(0, i)
+        val suffix = this.substring(i)
+        if (prefix.isValidByRule8(valid42s) && suffix.isValidByRule11(valid42s, valid31s)) {
+            return true
+        }
+    }
+    return false
+}
+
+fun String.isValidByRule8(valid42s: List<String>): Boolean {
+    if (this in valid42s) {
+        return true
+    } else {
+        for (i in 1 until this.length - 1) {
+            val prefix = this.substring(0, i)
+            val suffix = this.substring(i)
+            if (prefix in valid42s && suffix.isValidByRule8(valid42s)) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+fun String.isValidByRule11(valid42s: List<String>, valid31s: List<String>): Boolean {
+    for (i in 1 until this.length - 1) {
+        val prefix = this.substring(0, i)
+        val remainder = this.substring(i)
+        if (prefix in valid42s) {
+            if (remainder in valid31s) {
+                return true
+            }
+            for (j in 1 until remainder.length - 1) {
+                val middle = remainder.substring(0, j)
+                val suffix = remainder.substring(j)
+                if (middle.isValidByRule11(valid42s, valid31s) && suffix in valid31s) {
+                    return true
                 }
             }
-            null
         }
-        else -> null
     }
+    return false
 }
 
 fun main() {
     val input = MessageRule::javaClass.javaClass.classLoader.getResource("day19.txt")?.readText() ?: ""
     val (ruleInput, msgInput) = input.split("\n\n", limit = 2)
 
-    val rules: RuleSet = mutableMapOf()
+    val rules: MessageRuleSet = mutableMapOf()
     ruleInput.split("\n").forEach { str ->
         var (idx, reqs) = str.split(":", limit = 2)
         val i = idx.toInt()
         reqs = reqs.trim()
         if (reqs.startsWith('"')) {
-            rules[i] = CharRule(reqs[1])
+            rules[i] = ResolvedRule(listOf(reqs[1].toString()))
         } else {
             val seqs = reqs.split("|")
                 .map { seq ->
                     seq.trim().split(" ").map { it.toInt() }
                 }
-            rules[i] = SequenceRule(seqs)
+            rules[i] = UnresolvedRule(seqs)
         }
     }
 
-    val validMsgs1 = msgInput.split("\n").filter { rules.satisfiesRule(0, it) }
+    rules.resolve(0)
+    val ruleZero = rules[0] as ResolvedRule
+    val zeros = ruleZero.validMsgs
+
+    val validMsgs1 = msgInput.split("\n").filter { it in zeros }
     println(validMsgs1.size)
+
+    val fortyTwos = (rules[42] as ResolvedRule).validMsgs
+    val thirtyOnes = (rules[31] as ResolvedRule).validMsgs
+
+    val validMsgs2 = msgInput.split("\n").filter { it.isValidByRule0(fortyTwos, thirtyOnes) }
+    println(validMsgs2.size)
 }
