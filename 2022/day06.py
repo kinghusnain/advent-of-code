8a50465a5607223431a6c8387d90aea47ac05c64
Kevin Cheung <kevin@owmyneck.com>
2022-12-06 17:12:38 -0800

    Day 6 cleanup

--- 2022/day06.py
+++ 2022/day06.py
@@ -1,7 +1,7 @@
 """Advent of Code 2022, Day 6."""
 
 import doctest
-from collections.abc import Callable, Iterable
+from collections.abc import Callable
 
 
 def part1(problem_input: str) -> int:
@@ -9,10 +9,18 @@ def part1(problem_input: str) -> int:
 
     >>> part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
     7
+    >>> part1("bvwbjplbgvbhsrlpgdmjqwftvncz")
+    5
+    >>> part1("nppdvjthqldpwncqszvftbrmjlhg")
+    6
+    >>> part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+    10
+    >>> part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+    11
     """
-    for i in range(len(problem_input) - 4):
-        if len(set(problem_input[i : i + 4])) == 4:
-            return i + 4
+    for i in range(4, len(problem_input)):
+        if len(set(problem_input[i - 4 : i])) == 4:
+            return i
     raise ValueError
 
 
@@ -21,10 +29,18 @@ def part2(problem_input: str) -> int:
 
     >>> part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
     19
+    >>> part2("bvwbjplbgvbhsrlpgdmjqwftvncz")
+    23
+    >>> part2("nppdvjthqldpwncqszvftbrmjlhg")
+    23
+    >>> part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+    29
+    >>> part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+    26
     """
-    for i in range(len(problem_input) - 14):
-        if len(set(problem_input[i : i + 14])) == 14:
-            return i + 14
+    for i in range(14, len(problem_input)):
+        if len(set(problem_input[i - 14 : i])) == 14:
+            return i
     raise ValueError
 
 
